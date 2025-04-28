bd8289c0e9573f21306a1514f8f3e642729e876c
barkouchyass <barkouchyass@gmail.com>
2025-04-28 09:30:22 +0100

    install tailwind CSS

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2023 Walid Editors. All rights reserved.</p>
+        <p>
+          <a href="/privacy-policy" className="text-gray-400 hover:text-white">
+            Privacy Policy
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
